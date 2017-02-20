84e20acae5afdc5024bc7395d1624c7243585e20
root <root@vmi101819.contabo.host>
2017-02-20 13:17:59 +0100

    update app-template with proper settings

--- app_name/models.py
+++ app_name/models.py
@@ -1,3 +1,1 @@
-from django.dispatch import receiver
-from .signals import widgets
-from django.templatetags.static import static
+
--- app_name/urls.py
+++ app_name/urls.py
@@ -4,6 +4,6 @@ from . import views, APP_NAME
 
 urlpatterns = patterns('',
    # app urls
-   # url(r'^$', views.index, name='%s.index' % APP_NAME),
+   url(r'^$', views.index, name='%s.index' % APP_NAME),
 
 )
